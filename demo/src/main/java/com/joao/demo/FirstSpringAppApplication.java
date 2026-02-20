7b8ae9c1c8beffc9c7ca553b125acc7a02388b68
João Vitor Bosetti <joao.b2009@aluno.ifsc.edu.br>
2026-02-19 22:59:47 -0300

    Add files via upload

---
+++ demo/src/main/java/com/joao/demo/FirstSpringAppApplication.java
@@ -0,0 +1,15 @@
+package com.joao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//o aplication e a classe executavel do projeto
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/joao/demo/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.joao.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloworld(String name){
+        return "Hello World! " + name;
+    }
+}
